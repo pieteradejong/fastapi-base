dd331ce413ac1347cdf62079b87e4d77185d4436
Pieter de Jong <pieter.a.dejong@gmail.com>
2023-06-14 18:10:58 -0400

    simple fastapi app

---
+++ main.py
@@ -0,0 +1,14 @@
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"Hello": "World!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("app:app")
+
